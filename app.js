f3b86852a4e56b19032fdd72366b08bf48ae3d26
scepticae <samuel_leonard@comcast.net>
2020-08-31 16:00:28 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,37 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+var items = [];
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    var today = new Date();
+    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-us", options);
+
+    res.render("list", { kindOfDay: day, newListItems: items });
+});
+
+app.post("/", function(req, res){
+    var newItem = req.body.newItem;
+    items.push(newItem);
+
+    res.redirect("/");
+});
+
+app.listen(3000, function(){
+    console.log("Server running on port 3000");
+});
