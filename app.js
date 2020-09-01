c7d28f16fce6bbd2e2da0df6e5f8776c99ec7a0e
scepticae <samuel_leonard@comcast.net>
2020-09-01 15:17:11 -0400

    Add ejs templates for header and footer and create date module

--- app.js
+++ app.js
@@ -2,34 +2,40 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-var items = [];
+const items = [];
+const workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    var today = new Date();
-    
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
+    const day = date.getDate();
+    res.render("list", { listTitle: day, newListItems: items });
+});
 
-    var day = today.toLocaleDateString("en-us", options);
-
-    res.render("list", { kindOfDay: day, newListItems: items });
+app.get("/work", function(req, res){
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
 app.post("/", function(req, res){
-    var newItem = req.body.newItem;
-    items.push(newItem);
+    const newItem = req.body.newItem;
 
-    res.redirect("/");
+    if(req.body.list === "Work"){
+        workItems.push(newItem);
+        res.redirect("/work");
+    }else{
+        items.push(newItem);
+        res.redirect("/");
+    }
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
 });
 
 app.listen(3000, function(){
